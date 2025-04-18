b11af586d076a30451e372ccf96916d2f638e5a4
navoxio <116048393+navoxio@users.noreply.github.com>
2025-04-18 15:59:13 +0100

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,40 @@
+let fullName = document.getElementById("fullName");
+let email = document.getElementById("email");
+let message = document.getElementById("message");
+let phone = document.getElementById("phone");
+
+let sendBtn = document.getElementById("sendBtn");
+let successDiv = document.getElementById("successDiv");
+
+let count = 0;
+
+sendBtn.addEventListener("click", function (e) {
+  count++;
+
+  if (count > 2) {
+     
+    sendBtn.textContent = "You have sent too many messages. Try again later...";
+    return;
+  }
+
+  if (fullName.value == "" || email.value == "" || message.value == "") {
+    alert("Complete the form before submitting");
+    return;
+  }
+
+  sendBtn.disabled = true;
+  sendBtn.textContent = "Sending message...";
+   
+
+  axios
+    .post("https://api.pipbot.tech/v1/contact-mail", {
+      fullName: fullName.value,
+      uEmail: email.value,
+      message: message.value,
+      phone: phone
+    })
+    .then(function (response) {
+       sendBtn.textContent = "Message sent successfully..."
+    });
+});
+
