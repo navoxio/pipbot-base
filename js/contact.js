77b39dc02d13b49a996b530725192729de9a4d21
navoxio <116048393+navoxio@users.noreply.github.com>
2025-04-19 03:31:26 +0100

    Add files via upload

--- js/contact.js
+++ js/contact.js
@@ -25,13 +25,15 @@ sendBtn.addEventListener("click", function (e) {
   sendBtn.disabled = true;
   sendBtn.textContent = "Sending message...";
    
+  let timeStamp = new Date();
 
   axios
     .post("https://api.pipbot.tech/v1/contact-mail", {
       fullName: fullName.value,
       uEmail: email.value,
       message: message.value,
-      phone: phone
+      phone: phone,
+      timeStamp: String(timeStamp)
     })
     .then(function (response) {
        sendBtn.textContent = "Message sent successfully..."
